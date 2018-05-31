1d801f6dfde4552352e4a15e57f9dba994e6b9a6
dcdc2680 <39314528+dcdc2680@users.noreply.github.com>
2018-05-31 22:11:15 +0800

    指向非常數資料的非常數指標

---
+++ bubblesort.c
@@ -0,0 +1,19 @@
+void bubblesort( int *array,int size)
+{
+	int pass,j,hold;
+	for(pass=0;pass<size-1;++pass)
+	{
+		for(j=0;j<size-1;++j)
+		{
+			if(array[j]>array[j+1])
+			{
+					hold=array[j];
+	                array[j]=array[j+1];
+	                array[j+1]=hold;
+				
+			}
+		}
+	}
+	return;
+}
+
